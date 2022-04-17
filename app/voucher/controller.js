ed80f3a952a14d09892b6cd6783b9670dfaebc39
unknown <nafhanghifari@gmail.com>
2022-04-17 15:37:09 +0700

    delete-vooucher

--- app/voucher/controller.js
+++ app/voucher/controller.js
@@ -95,58 +95,139 @@ module.exports = {
       res.redirect('/nominal')
     }
   },
-  // viewEdit: async(req, res)=> {
-  //     try {
-  //         const { id } = req.params
+  viewEdit: async(req, res)=> {
+      try {
+          const { id } = req.params
 
-  //         const category = await Category.findOne({_id: id})
-          
+          const category = await Category.find()
+          const nominal = await Nominal.find()
+          const voucher = await Voucher.findOne({_id: id})
+          .populate('category')
+          .populate('nominals')
 
-  //         res.render('admin/category/edit', {
-  //             category
-  //         })
+          res.render('admin/voucher/edit', {
+              voucher,
+              nominal,
+              category
+          })
 
-  //     } catch (err) {
-  //          req.flash('alertMessage', `${err.message}`)
-  //         req.flash('alertStatus', 'danger')
-  //         res.redirect('/category')
-  //     }
-  // },
-  // actionEdit: async(req, res)=> {
-  //     try {
-  //         const { id } = req.params
-  //         const {name} = req.body
+      } catch (err) {
+           req.flash('alertMessage', `${err.message}`)
+          req.flash('alertStatus', 'danger')
+          res.redirect('/voucher')
+      }
+  },
+  actionEdit: async (req, res) => {
+    try {
+      const { id } = req.params
+      const { name, category, nominals } = req.body
 
-          
-  //         const category = await Category.findByIdAndUpdate({
-  //             _id: id
-  //         }, {name})
+      if(req.file){
+        let tmp_path= req.file.path;
+        let originaExt = req.file.originalname.split('.')[req.file.originalname.split('.').length - 1];
+        let filename = req.file.filename + '.' + originaExt;
+        let target_path = path.resolve(config.rootPath, `public/uploads/${filename}`)
 
-  //         req.flash('alertMessage', 'Berhasil ubah kategori')
-  //         req.flash('alertStatus', 'success')
-  //         res.redirect('/category')
+        const src = fs.createReadStream(tmp_path)
+        const dest = fs.createWriteStream(target_path)
 
-  //     } catch (err) {
-  //          req.flash('alertMessage', `${err.message}`)
-  //         req.flash('alertStatus', 'danger')
-  //         res.redirect('/category')
-  //     }
-  // },
-  // actionDelete: async(req, res)=> {
-  //     try {
-  //         const { id } = req.params
+        src.pipe(dest)
 
-  //         const category = await Category.findByIdAndDelete({
-  //             _id: id
-  //         })
+        src.on('end', async ()=>{
+          try {
 
-  //         req.flash('alertMessage', 'Berhasil hapus kategori')
-  //         req.flash('alertStatus', 'success')
-  //         res.redirect('/category')
-  //     } catch (err) {
-  //          req.flash('alertMessage', `${err.message}`)
-  //         req.flash('alertStatus', 'danger')
-  //         res.redirect('/category')
-  //     }
-  // },
+            const voucher = await Voucher.findOne({_id: id})
+
+            let currentImage = `${config.rootPath}/public/uploads/${voucher.thumbnial}`;
+            if(fs.existsSync(currentImage)){
+              fs.unlinkSync(currentImage)
+            }
+
+            await Voucher.findOneAndUpdate({
+              _id : id
+            },{
+              name,
+              category,
+              nominals,
+              thumbnial: filename
+            })
+            
+
+            req.flash('alertMessage', "Berhasil ubah voucher")
+            req.flash('alertStatus', "success")
+      
+            res.redirect('/voucher')
+            
+          } catch (err) {
+            req.flash('alertMessage', `${err.message}`)
+            req.flash('alertStatus', 'danger')
+            res.redirect('/voucher')
+          }
+        })
+      }else{
+        await Voucher.findOneAndUpdate({
+          _id : id
+        },{
+          name,
+          category,
+          nominals,
+        })
+        
+        req.flash('alertMessage', "Berhasil ubah voucher")
+        req.flash('alertStatus', "success")
+  
+        res.redirect('/voucher')
+      }
+
+    } catch (err) {
+      req.flash('alertMessage', `${err.message}`)
+      req.flash('alertStatus', 'danger')
+      res.redirect('/nominal')
+    }
+  },
+  actionDelete: async(req, res)=> {
+      try {
+          const { id } = req.params
+
+          const voucher = await Voucher.findByIdAndDelete({
+              _id: id
+          })
+          let currentImage = `${config.rootPath}/public/uploads/${voucher.thumbnial}`;
+            if(fs.existsSync(currentImage)){
+              fs.unlinkSync(currentImage)
+            }
+
+          req.flash('alertMessage', 'Berhasil hapus kategori')
+          req.flash('alertStatus', 'success')
+          res.redirect('/voucher')
+      } catch (err) {
+          req.flash('alertMessage', `${err.message}`)
+          req.flash('alertStatus', 'danger')
+          res.redirect('/voucher')
+      }
+  },
+  actionStatus : async (req, res)=>{
+    try {
+      const { id } = req.params
+      let voucher = await Voucher.findOne({_id: id})
+
+      let status = voucher.status === 'Y' ? 'N' : 'Y'
+
+      voucher = await Voucher.findOneAndUpdate({
+        _id : id
+      }, {status})
+      
+      req.flash('alertMessage', "Berhasil ubah status")
+      req.flash('alertStatus', "success")
+
+      res.redirect('/voucher')
+
+      
+    } catch (err) {
+      req.flash('alertMessage', `${err.message}`)
+      req.flash('alertStatus', 'danger')
+      res.redirect('/voucher')
+    }
+  }
+
 }
--- app/voucher/router.js
+++ app/voucher/router.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const {index, viewCreate, actionCreate} = require('./controller')
+const {index, viewCreate, actionCreate, viewEdit, actionEdit, actionDelete, actionStatus} = require('./controller')
 const multer = require('multer')
 const os = require('os')
 
@@ -8,8 +8,9 @@ const os = require('os')
 router.get('/', index);
 router.get('/create', viewCreate);
 router.post('/create',multer({ dest: os.tmpdir() }).single('image'), actionCreate);
-// router.get('/edit/:id', viewEdit);
-// router.put('/edit/:id', actionEdit);
-// router.delete('/delete/:id', actionDelete);
+router.get('/edit/:id', viewEdit);
+router.put('/edit/:id', multer({ dest: os.tmpdir() }).single('image'), actionEdit);
+router.delete('/delete/:id', actionDelete);
+router.put('/status/:id', actionStatus);
 
 module.exports = router;
