e494e8516f683bf057a9bf50ca812ce0c1f8bf7c
unknown <nafhanghifari@gmail.com>
2022-06-28 22:36:52 +0700

    -Membuat-Api-SignIn-dan-Category-

--- app/auth/controller.js
+++ app/auth/controller.js
@@ -2,9 +2,11 @@ const Player = require('../player/model')
 const path = require('path')
 const fs = require('fs')
 const config = require('../../config')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 module.exports = {
-    signup: async (req, res) => {
+    signup: async (req, res, next) => {
         try {
             const payload = req.body
 
@@ -66,5 +68,45 @@ module.exports = {
             }
             next(err)
         }
+    },
+    signin: async(req, res, next) => {
+        const {email, password} = req.body
+
+        Player.findOne({email: email}).then((player) => {
+            if (player){    
+                const checkPassword = bcrypt.compareSync(password, player.password)
+                if (checkPassword){
+                  const token = jwt.sign({
+                    player: {
+                        id: player.id,
+                        username: player.username,
+                        email: player.email,
+                        nama: player.nama,
+                        phoneNumber: player.phoneNumber,
+                        avatar: player.avatar,
+                    }
+                  }, config.jwtKey)
+
+                    res.status(200).json({
+                        data: {token}
+                    })
+                } else {
+                    res.status(403).json({
+                        message: 'Wrong password'
+                })
+                }
+            } else {
+                res.status(403).json({
+                    message: 'Email tidak ditemukan'
+                })
+            }
+
+        }).catch((err) => {
+            res.status(500).json({
+                message: err.message || 'Server error'
+            })
+
+            next()
+        })
     }
 }
--- app/auth/router.js
+++ app/auth/router.js
@@ -2,9 +2,10 @@ var express = require('express');
 var router = express.Router();
 const multer = require('multer')
 const os = require('os')
-const {signup} = require('./controller')
+const {signup, signin} = require('./controller')
 
 
 router.post('/signup',multer({ dest: os.tmpdir() }).single('image'), signup);
+router.post('/signin', signin);
 
 module.exports = router;
